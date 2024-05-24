22910a7ad2eaaf65886f957e599a45b9dafae885
mdhcodes <mdhcodes@gmail.com>
2024-05-24 12:47:24 -0400

    Update models.py and admin.py

--- parks/admin.py
+++ parks/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Lesson
+
+
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+class LessonAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Lesson, LessonAdmin)
--- parks/models.py
+++ parks/models.py
@@ -1,3 +1,5 @@
+# https://docs.djangoproject.com/en/5.0/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
+# If you’re starting a new project, it’s highly recommended to set up a custom user model, even if the default User model is sufficient for you. This model behaves identically to the default user model, but you’ll be able to customize it in the future if the need arises.
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -7,8 +9,23 @@ class User(AbstractUser):
     pass
 
 
+# When a user saves a lesson, a model is created from the json data and stored in the Lesson table.
 class Lesson(models.Model):
-    park = models.CharField(max_length=10) # park code
+    url = models.URLField()
+
+    title = models.TextField()
+
+    parks = models.JSONField() # The parkCode is stored in the parks list.
+
+    questionObjective = models.TextField()
+
+    gradeLevel = models.TextField()
+
+    commonCore = models.JSONField()
+
+    subject = models.JSONField()
+
+    duration = models.TextField()
 
     notes = models.TextField(blank=True)
 
@@ -17,9 +34,9 @@ class Lesson(models.Model):
     # https://www.geeksforgeeks.org/filefield-django-models/
     doc_upload = models.FileField(upload_to='uploads/%Y/%m/%d', blank=True)
 
-    educator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='educator')
 
     date = models.DateTimeField(auto_now=False,  auto_now_add=True)
 
     def __str__(self):
-        return f'{self.park} / {self.educator}'
+        return f'{self.title} / {self.educator}'
