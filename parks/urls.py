1c52a318f7d1846f7e80b4716bc0246cfbf26960
mdhcodes <mdhcodes@gmail.com>
2024-06-03 07:20:37 -0400

    Add register and login functionality

--- parks/urls.py
+++ parks/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 
 from . import views
 
+# https://stackoverflow.com/questions/39316948/typeerror-login-takes-1-positional-argument-but-2-were-given
+# The app login and logout view must be renamed to avoid clashing with the Django auth login and logout functions.
 urlpatterns = [
     path('', views.index, name='index'),
-    path('parks/', views.parks, name='parks'),
+    path("login", views.login_user, name="login"),
+    path("logout", views.logout_user, name="logout"),
+    path("register", views.register, name="register"),
+    path('parks', views.parks, name='parks'),
 ]
--- parks/views.py
+++ parks/views.py
@@ -1,7 +1,14 @@
 import json
 import requests
-from django.http import JsonResponse, HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from django.contrib.auth.decorators import login_required
+
+from .models import User
 
 from learning.settings import NP_API_KEY
 
@@ -15,6 +22,83 @@ def index(request):
     
     return render(request, 'parks/index.html', context)
 
+
+# https://docs.djangoproject.com/en/5.0/topics/auth/default/#how-to-log-a-user-in
+def login_user(request):
+    if request.method == 'POST':
+
+        # Attempt to sign user in
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication was successful
+        if user is not None:
+            login(request, user)
+
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            context = {
+                'message': 'Invalid username and/or password.'
+            }
+            return render(request, 'parks/login.html', context)
+    else:
+        return render(request, 'parks/login.html')
+
+
+# https://docs.djangoproject.com/en/5.0/topics/auth/default/#how-to-log-a-user-out
+def logout_user(request):
+    logout(request)        
+    return HttpResponseRedirect(reverse('index'))
+
+
+def register(request):
+    if request.method == 'POST':
+        # Ensure username submitted
+        if not request.POST['username']:
+            context = {
+                'message' : 'Please provide username'
+            }
+            return render(request, 'parks/register.html', context)
+        
+        # Ensure email submitted
+        if not request.POST['email']:
+            context = {
+                'message' : 'Please provide email'
+            }
+            return render(request, 'parks/register.html', context)
+
+        # Ensure password submitted
+        if not request.POST['password']:
+            context = {
+                'message' : 'Please provide password'
+            }
+            return render(request, 'parks/register.html', context)
+
+        # Ensure password confirmation
+        if not request.POST['confirmation']:
+            context = {
+                'message' : 'Please provide password confirmation'
+            }
+            return render(request, 'parks/register.html', context)        
+
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            context = {
+                'message': 'Username already taken.'
+            }
+            return render(request, 'parks/register.html', context)
+        login(request, user)
+        return HttpResponseRedirect(reverse('index'))
+    else:
+        return render(request, 'parks/register.html')
 
 
 def parks(request):
@@ -39,3 +123,6 @@ def parks(request):
         
 
     
+# @login_required
+# def save_lesson():
+    # pass
